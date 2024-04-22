cd04d06260eeaad65c9d88cca9a615310533ce0b
Bogdan Romanko <bromankoya@gmail.com>
2024-04-22 09:41:25 +0300

    user with jwt

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model')
 const apiError = require('../exceptions/server.error')
 const crypto = require('crypto')
+const tokenService = require('./token.service')
 
 class UserService {
 
@@ -17,37 +18,58 @@ class UserService {
     }
 
     async registration(name, password, role) {
-        const salt = crypto.randomBytes(16).toString('hex')
-        const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex')
+        const hashPassword = this.hashPassword(password)
 
-        return await userModel.registration(name, hash, salt, role)
+        await userModel.registration(name, hashPassword.hash, hashPassword.salt, role)
+
+        const user = await userModel.getUserByName(name)
+
+        const tokens = await tokenService.generateTokens({userId: user.id, userName: user.name, role: user.role})
+        await tokenService.saveToken(user.id, tokens.refreshToken)
+
+        return tokens
     }
 
     async login(name, password) {
         try {
-            const data = userModel.getUserByName(name)
+            const user = await userModel.getUserByName(name)
 
-            if (!data) {
+            if (!user)
                 throw new apiError(404, `User with name "${name}" not found`)
-            }
 
-            var hash = crypto.pbkdf2Sync(password, data.salt, 1000, 64, 'sha512').toString('hex')
-            if (data.hash === hash) {
+            const hashPassword = this.hashPassword(password)
 
-            } else {
+            if (user.hash === hashPassword.hash) {
+                const tokens = await tokenService.generateTokens({userId: user.id, userName: user.name, role: user.role})
+                await tokenService.saveToken(user.id, tokens.refreshToken)
+            } else
                 throw new apiError(404, "Password not valid")
-            }
         } catch (e) {
             next(e)
         }
     }
 
     async refresh(refreshToken) {
+        if (!refreshToken)
+            throw apiError.UnauthorizedError()
 
+        const userData = await tokenService.validateRefreshToken(refreshToken)
+        const tokenDB = await tokenService.findToken(refreshToken)
+
+        if (!userData || !tokenDB)
+            throw apiError.UnauthorizedError()
+
+        const user = await userModel.getUserById(userData.id)
+        const tokens = await tokenService.generateTokens({userId: user.id, userName: user.name, role: user.role})
+        return tokens
     }
 
-    async logout() {
+    async logout(token) {
+        if (!token)
+            throw apiError.UnauthorizedError()
 
+        await tokenService.removeToken(token)
+        return true
     }
 
     async deleteUser(id) {
@@ -55,10 +77,42 @@ class UserService {
     }
 
     async updateUser(id, name, password, role) {
+        const oldUser = await userModel.getUserById(parseInt(id))
+
+        if (!oldUser)
+            throw new apiError(404, `User with name "${name}" not found`)
+
+        const newUser = []
+
+        if (name)
+            newUser.push({name: name})
+        else
+            newUser.push({name: oldUser.name})
+        
+        if (password)
+            newUser.push({password: this.hashPassword(password)})
+        else
+            newUser.push({password: {hash: oldUser.hash, salt: oldUser.salt}})
+
+        if (role)
+            newUser.push({role: role})
+        else
+            newUser.push({role: oldUser.role})
+        
+        const user = await userModel.updateUser(parseInt(id), newUser.name, newUser.password.hash, newUser.password.salt, newUser.role)
+
+        if (!user)
+            throw new apiError(404, `User with name "${name}" not found`)
+
+        const tokens = await tokenService.generateTokens({userId: user.id, userName: user.name, role: user.role})
+        
+        return tokens
+    }
+
+    hashPassword(password) {
         const salt = crypto.randomBytes(16).toString('hex')
         const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex')
-
-        return await userModel.updateUser(parseInt(id), name, hash, salt, role)
+        return {hash, salt}
     }
 
 }
