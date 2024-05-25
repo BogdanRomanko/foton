01cea53a13b9fb26f40ff22da7e9895873f1b1bf
Zendau <cowalyow2012@gmail.com>
2024-05-25 21:07:10 +0300

    feat(client): skeleton product template

---
+++ client/components/products/skeleton/card.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="products-section-content-card">
+    <USkeleton class="h-60 w-20" />
+    <div class="mt-3 space-y-2 text-center">
+      <USkeleton class="h-6 w-20" />
+      <USkeleton class="h-40 w-20" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ client/components/products/skeleton/list.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="products">
+    <div class="products-section-content">
+      <ProductsSkeletonCard v-for="item of 10" :key="item" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
