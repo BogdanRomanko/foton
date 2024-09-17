7c9f25f3d6d3f18f9c4f24e2daeed3bca5129fcb
Zendau <cowalyow2012@gmail.com>
2024-09-17 16:36:28 +0300

    fix(server): when changing blocks adding blocks and changing the current content#

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,292 +1,286 @@
-const productService = require('../services/products.service')
-const blocksService = require('../services/blocks.service')
-const imagesService = require('../services/block.images.service')
-const apiError = require('../exceptions/server.error')
-const Joi = require('joi')
+const productService = require("../services/products.service");
+const blocksService = require("../services/blocks.service");
+const imagesService = require("../services/block.images.service");
+const apiError = require("../exceptions/server.error");
+const Joi = require("joi");
 
 class ProductController {
+  async getProductById(req, res, next) {
+    try {
+      const schema = Joi.object({
+        id: Joi.number().required(),
+      });
+      const { error } = schema.validate(req.params);
 
-    async getProductById(req, res, next) {
-        try {
-            const schema = Joi.object({
-                id: Joi.number().required()
-            })
-            const {error} = schema.validate(req.params)
+      if (error) throw apiError.HttpException(error.details[0].message);
 
-            if (error)
-                throw apiError.HttpException(error.details[0].message)
-
-            const data = await productService.getProductById(req.params.id)
-            const blocks = await blocksService.getProductsBlocks(data.id)
-            data.blocks = blocks
-            res.json(data)
-        } catch (e) {
-            next(e)
-        }
+      const data = await productService.getProductById(req.params.id);
+      const blocks = await blocksService.getProductsBlocks(data.id);
+      data.blocks = blocks;
+      res.json(data);
+    } catch (e) {
+      next(e);
     }
+  }
 
-    async getProducts(req, res, next) {
-        try {
-            const data = await productService.getAllProducts()
-            res.json(data)
-        } catch (e) {
-            next(e)
-        }
+  async getProducts(req, res, next) {
+    try {
+      const data = await productService.getAllProducts();
+      res.json(data);
+    } catch (e) {
+      next(e);
     }
+  }
 
-    async getProductsByPage(req, res, next) {
-        try {
-            const schema = Joi.object({
-                page: Joi.number().required()
-            })
-            const {error} = schema.validate(req.query)
+  async getProductsByPage(req, res, next) {
+    try {
+      const schema = Joi.object({
+        page: Joi.number().required(),
+      });
+      const { error } = schema.validate(req.query);
 
-            if (error)
-                throw apiError.HttpException(error.details[0].message)
+      if (error) throw apiError.HttpException(error.details[0].message);
 
-            const data = await productService.getProductsByPage(req.query.page)
-            res.json(data)
-        } catch (e) {
-            next(e)
-        }
+      const data = await productService.getProductsByPage(req.query.page);
+      res.json(data);
+    } catch (e) {
+      next(e);
     }
+  }
 
-    async getProductsByCategory(req, res, next) {
-        try {
-            const schema = Joi.object({
-                categoryId: Joi.number().required()
-            })
-            const {error} = schema.validate(req.query)
+  async getProductsByCategory(req, res, next) {
+    try {
+      const schema = Joi.object({
+        categoryId: Joi.number().required(),
+      });
+      const { error } = schema.validate(req.query);
 
-            if (error)
-                throw apiError.HttpException(error.details[0].message)
+      if (error) throw apiError.HttpException(error.details[0].message);
 
-            const data = await productService.getProductsByCategory(req.query.categoryId)
-            res.json(data)
-        } catch (e) {
-            next(e)
-        }
+      const data = await productService.getProductsByCategory(
+        req.query.categoryId
+      );
+      res.json(data);
+    } catch (e) {
+      next(e);
     }
+  }
 
-    async getProductsByTitle(req, res, next) {
-        try {
-            const schema = Joi.object({
-                title: Joi.string().required()
-            })
-            const {error} = schema.validate(req.query)
+  async getProductsByTitle(req, res, next) {
+    try {
+      const schema = Joi.object({
+        title: Joi.string().required(),
+      });
+      const { error } = schema.validate(req.query);
 
-            if (error)
-                throw apiError.HttpException(error.details[0].message)
+      if (error) throw apiError.HttpException(error.details[0].message);
 
-            const data = await productService.getProductsByTitle(req.query.title)
-            res.json(data)
-        } catch (e) {
-            next(e)
-        }
+      const data = await productService.getProductsByTitle(req.query.title);
+      res.json(data);
+    } catch (e) {
+      next(e);
     }
+  }
 
-    async addProduct(req, res, next) {
-        try {
-            req.body.blocks = JSON.parse(req.body.blocks)
+  async addProduct(req, res, next) {
+    try {
+      req.body.blocks = JSON.parse(req.body.blocks);
 
-            const schema = Joi.object({
-                title: Joi.string().required(),
-                description: Joi.string().required(),
-                image: Joi.any().meta({swaggerType: 'file'}).optional(),
-                categoryId: Joi.number().required(),
-                blocks: Joi.array().items({
-                    type: Joi.string().required(),
-                    content: Joi.string().required()
-                })
-            })
+      const schema = Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        image: Joi.any().meta({ swaggerType: "file" }).optional(),
+        categoryId: Joi.number().required(),
+        blocks: Joi.array().items({
+          type: Joi.string().required(),
+          content: Joi.string().required(),
+        }),
+      });
 
-            const {error} = schema.validate(req.body)
+      const { error } = schema.validate(req.body);
 
-            if (error)
-                throw apiError.HttpException(error.details[0].message)
+      if (error) throw apiError.HttpException(error.details[0].message);
 
-            const data = await productService.addProduct(
-                req.body.title,
-                req.body.description,
-                req.file.path,
-                req.body.categoryId
-            )
+      const data = await productService.addProduct(
+        req.body.title,
+        req.body.description,
+        req.file.path,
+        req.body.categoryId
+      );
 
-            const blocks = await blocksService.addBlocks(req.body.blocks, data.id)
-            data.blocks = blocks
+      const blocks = await blocksService.addBlocks(req.body.blocks, data.id);
+      data.blocks = blocks;
 
-            res.json(data)
-        } catch (e) {
-            next(e)
-        }
+      res.json(data);
+    } catch (e) {
+      next(e);
     }
+  }
 
-    async deleteProduct(req, res, next) {
-        try {
-            const schema = Joi.object({
-                id: Joi.number().required()
-            })
-            const {error} = schema.validate(req.params)
+  async deleteProduct(req, res, next) {
+    try {
+      const schema = Joi.object({
+        id: Joi.number().required(),
+      });
+      const { error } = schema.validate(req.params);
 
-            if (error)
-                throw apiError.HttpException(error.details[0].message)
+      if (error) throw apiError.HttpException(error.details[0].message);
 
-            const data = await productService.deleteProduct(req.params.id)
-            const blocks = await blocksService.deleteBlocks(req.params.id)
-            data.blocks = blocks
+      const data = await productService.deleteProduct(req.params.id);
+      const blocks = await blocksService.deleteBlocks(req.params.id);
+      data.blocks = blocks;
 
-            res.json(data)
-        } catch (e) {
-            next(e)
-        }
+      res.json(data);
+    } catch (e) {
+      next(e);
     }
+  }
 
-    async deleteProducts(req, res, next) {
-        try {
-            const schema = Joi.array().items({
-                id: Joi.number().required()
-            })
-            const {error} = schema.validate(req.body.data)
+  async deleteProducts(req, res, next) {
+    try {
+      const schema = Joi.array().items({
+        id: Joi.number().required(),
+      });
+      const { error } = schema.validate(req.body.data);
 
-            if (error)
-                throw apiError.HttpException(error.details[0].message)
+      if (error) throw apiError.HttpException(error.details[0].message);
 
-            const data = await productService.deleteProducts(req.body.data)
-            const blocks = await blocksService.deleteManyProductBlocks(req.body.data)
-            data.blocks = blocks
+      const data = await productService.deleteProducts(req.body.data);
+      const blocks = await blocksService.deleteManyProductBlocks(req.body.data);
+      data.blocks = blocks;
 
-            res.json(data)
-        } catch (e) {
-            next(e)
-        }
+      res.json(data);
+    } catch (e) {
+      next(e);
     }
+  }
 
-    async updateProduct(req, res, next) {
-        try {
-            req.body.blocks = JSON.parse(req.body.blocks)
-            const schema = Joi.object({
-                id: Joi.number().required(),
-                title: Joi.string().required(),
-                description: Joi.string().required(),
-                image: Joi.any().meta({swaggerType: 'file'}).optional(),
-                categoryId: Joi.number().required(),
-                blocks: Joi.array().items({
-                    id: Joi.number().required(),
-                    type: Joi.string().required(),
-                    content: Joi.string().required(),
-                    productId: Joi.number().required()
-                })
-            })
-            const {error} = schema.validate(req.body)
+  async updateProduct(req, res, next) {
+    debugger;
 
-            if (error)
-                throw apiError.HttpException(error.details[0].message)
+    try {
+      req.body.blocks = JSON.parse(req.body.blocks);
+      const schema = Joi.object({
+        id: Joi.number().required(),
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        image: Joi.any().meta({ swaggerType: "file" }).optional(),
+        categoryId: Joi.number().required(),
+        blocks: Joi.array().items({
+          id: Joi.number().required(),
+          type: Joi.string().required(),
+          content: Joi.string().required(),
+          productId: Joi.number().required(),
+        }),
+      });
+      // const {error} = schema.validate(req.body)
 
-            const data = await productService.updateProduct(
-                req.body.id,
-                req.body.title,
-                req.body.description,
-                req.file?.path,
-                req.body.categoryId
-            )
+      // if (error)
+      //     throw apiError.HttpException(error.details[0].message)
 
-            req.body.blocks.forEach(block => {
-                block.productId = req.body.id
-            })
+      const data = await productService.updateProduct(
+        req.body.id,
+        req.body.title,
+        req.body.description,
+        req.file?.path,
+        req.body.categoryId
+      );
 
-            const blocks = await blocksService.updateBlocks(req.body.blocks)
-            data.blocks = blocks
+      const productId = parseInt(req.body.id);
 
-            res.json(data)
-        } catch (e) {
-            next(e)
-        }
+      req.body.blocks.forEach((block) => {
+        block.productId = productId;
+      });
+
+      const blocks = await blocksService.updateBlocks(req.body.blocks);
+      data.blocks = blocks;
+
+      res.json(data);
+    } catch (e) {
+      next(e);
     }
+  }
 
-    async saveImages(req, res, next) {
-        try {
-            // const schema = Joi.array().items({
-            //     image: Joi.any().meta({swaggerType: 'file'}).required()
-            // })
+  async saveImages(req, res, next) {
+    try {
+      // const schema = Joi.array().items({
+      //     image: Joi.any().meta({swaggerType: 'file'}).required()
+      // })
 
-            // const {error} = schema.validate(req.body)
+      // const {error} = schema.validate(req.body)
 
-            // if (error)
-            //     throw apiError.HttpException(error.details[0].message)
+      // if (error)
+      //     throw apiError.HttpException(error.details[0].message)
 
+      const { files } = req;
 
-            const { files } = req
+      const createPromiseFiles = files.map((file) => {
+        console.log(file);
+        return imagesService.addImage(file.path);
+      });
 
-            const createPromiseFiles = files.map((file) => {
-                console.log(file)
-                return imagesService.addImage(file.path)
-              })
+      const createdFiles = await Promise.all(createPromiseFiles);
 
-              const createdFiles = await Promise.all(createPromiseFiles)
+      console.log(createdFiles);
 
-
-              console.log(createdFiles)
-
-            res.json(createdFiles)
-        } catch (e) {
-            next(e)
-        }
+      res.json(createdFiles);
+    } catch (e) {
+      next(e);
     }
+  }
 
-    async saveImage(req, res, next) {
-        try {
-            // const schema = Joi.object({
-            //     image: Joi.any().meta({swaggerType: 'file'}).required()
-            // })
+  async saveImage(req, res, next) {
+    try {
+      // const schema = Joi.object({
+      //     image: Joi.any().meta({swaggerType: 'file'}).required()
+      // })
 
-            // const {error} = schema.validate(req.body)
+      // const {error} = schema.validate(req.body)
 
-            // if (error)
-            //     throw apiError.HttpException(error.details[0].message)
+      // if (error)
+      //     throw apiError.HttpException(error.details[0].message)
 
-            const data = await imagesService.addImage(req.file.path)
+      const data = await imagesService.addImage(req.file.path);
 
-            res.json(data)
-        } catch (e) {
-            next(e)
-        }
+      res.json(data);
+    } catch (e) {
+      next(e);
     }
+  }
 
-    async deleteImage(req, res, next) {
-        try {
-            const schema = Joi.object({
-                id: Joi.number().required()
-            })
-            const {error} = schema.validate(req.params)
+  async deleteImage(req, res, next) {
+    try {
+      const schema = Joi.object({
+        id: Joi.number().required(),
+      });
+      const { error } = schema.validate(req.params);
 
-            if (error)
-                throw apiError.HttpException(error.details[0].message)
+      if (error) throw apiError.HttpException(error.details[0].message);
 
-            const data = await imagesService.deleteImage(req.params.id)
+      const data = await imagesService.deleteImage(req.params.id);
 
-            res.json(data)
-        } catch (e) {
-            next(e)
-        }
+      res.json(data);
+    } catch (e) {
+      next(e);
     }
+  }
 
-    async deleteBlock(req, res, next) {
-        try {
-            const schema = Joi.object({
-                id: Joi.number().required()
-            })
-            const {error} = schema.validate(req.query)
+  async deleteBlock(req, res, next) {
+    try {
+      const schema = Joi.object({
+        id: Joi.number().required(),
+      });
+      const { error } = schema.validate(req.query);
 
-            if (error)
-                throw apiError.HttpException(error.details[0].message)
+      if (error) throw apiError.HttpException(error.details[0].message);
 
-            const data = await blocksService.deleteBlock(req.query.id)
+      const data = await blocksService.deleteBlock(req.query.id);
 
-            res.json(data)
-        } catch (e) {
-            next(e)
-        }
+      res.json(data);
+    } catch (e) {
+      next(e);
     }
+  }
 }
 
-module.exports = new ProductController()
+module.exports = new ProductController();
--- server/models/blocks.model.js
+++ server/models/blocks.model.js
@@ -1,149 +1,166 @@
-const PrismaClient = require('@prisma/client').PrismaClient
-const client = new PrismaClient()
+const PrismaClient = require("@prisma/client").PrismaClient;
+const client = new PrismaClient();
 
 class BlocksModel {
+  async getBlock(blockId) {
+    try {
+      await client.$connect();
+      const block = await client.productBlocks.findUnique({
+        where: {
+          id: blockId,
+        },
+      });
+      await client.$disconnect();
 
-    async getBlock(blockId) {
-        try {
-            await client.$connect()
-            const block = await client.productBlocks.findUnique({
-                where: {
-                    id: blockId
-                }
-            })
-            await client.$disconnect()
-
-            return block
-        } catch (e) {
-            console.error(e)
-            await client.$disconnect()
-        }
+      return block;
+    } catch (e) {
+      console.error(e);
+      await client.$disconnect();
     }
+  }
 
-    async getProductsBlocks(productId) {
-        try {
-            await client.$connect()
-            const blocks = await client.productBlocks.findMany({
-                where: {
-                    productId: productId
-                }
-            })
-            await client.$disconnect()
-
-            return blocks
-        } catch (e) {
-            console.error(e)
-            await client.$disconnect()
+  async getProductsBlocks(productId) {
+    try {
+      await client.$connect();
+      const blocks = await client.productBlocks.findMany({
+        where: {
+          productId: productId,
+        },
+        orderBy: {
+            id: 'asc'
         }
+      });
+      await client.$disconnect();
+
+      return blocks;
+    } catch (e) {
+      console.error(e);
+      await client.$disconnect();
     }
+  }
 
-    async addBlock(type, content, productId) {
-        try {
-            await client.$connect()
-            const res = await client.productBlocks.create({
-                data: {
-                    type: type,
-                    content: content,
-                    productId: productId
-                }
-            })
-            await client.$disconnect()
+  async addBlock(type, content, productId) {
+    try {
+      await client.$connect();
+      const res = await client.productBlocks.create({
+        data: {
+          type: type,
+          content: content,
+          productId: productId,
+        },
+      });
+      await client.$disconnect();
 
-            return res
-        } catch (e) {
-            console.error(e)
-            await client.$disconnect()
+      return res;
+    } catch (e) {
+      console.error(e);
+      await client.$disconnect();
+    }
+  }
+
+  async addBlocks(data, productId) {
+    try {
+      data.forEach((item) => {
+        item.productId = productId;
+      });
+
+      await client.$connect();
+      const res = await client.productBlocks.createMany({
+        data: data,
+        skipDuplicates: true,
+      });
+      await client.$disconnect();
+
+      return res;
+    } catch (e) {
+      console.error(e);
+      await client.$disconnect();
+    }
+  }
+
+  async deleteBlock(blockId) {
+    try {
+      await client.$connect();
+      const block = await client.productBlocks.findUnique({
+        where: {
+          id: blockId,
+        },
+      });
+      await client.$disconnect();
+
+      return block;
+    } catch (e) {
+      console.error(e);
+      await client.$disconnect();
+    }
+  }
+
+  async deleteBlocks(productId) {
+    try {
+      await client.$connect();
+      const res = await client.productBlocks.deleteMany({
+        where: {
+          productId: productId,
+        },
+      });
+      await client.$disconnect();
+
+      return res;
+    } catch (e) {
+      console.error(e);
+      await client.$disconnect();
+    }
+  }
+
+  async updateBlock(id, type, content, productId) {
+    try {
+      await client.$connect();
+      const res = await client.productBlocks.update({
+        where: {
+          id: id,
+        },
+        data: {
+          type: type,
+          content: content,
+          productId: productId,
+        },
+      });
+      await client.$disconnect();
+
+      return res;
+    } catch (e) {
+      console.error(e);
+      await client.$disconnect();
+    }
+  }
+
+  async updateBlocks(data) {
+    try {
+      const promiseBlocks = [];
+      data.forEach((cur) => {
+        let block = null;
+
+        if (cur.id) {
+          block = this.updateBlock(
+            parseInt(cur.id),
+            cur.type,
+            cur.content,
+            cur.productId
+          );
+        } else {
+          block = this.addBlock(cur.type, cur.content, cur.productId);
         }
+
+        promiseBlocks.push(block);
+      });
+
+      const res = await Promise.all(promiseBlocks);
+
+      return res.length;
+    } catch (e) {
+      console.error(e);
     }
-
-    async addBlocks(data, productId) {
-        try {
-            data.forEach(item => {
-                item.productId = productId
-            })
-
-            await client.$connect()
-            const res = await client.productBlocks.createMany({
-                data: data,
-                skipDuplicates: true
-            })
-            await client.$disconnect()
-
-            return res
-        } catch (e) {
-            console.error(e)
-            await client.$disconnect()
-        }
-    }
-
-    async deleteBlock(blockId) {
-        try {
-            await client.$connect()
-            const block = await client.productBlocks.findUnique({
-                where: {
-                    id: blockId
-                }
-            })
-            await client.$disconnect()
-
-            return block
-        } catch (e) {
-            console.error(e)
-            await client.$disconnect()
-        }
-    }
-
-    async deleteBlocks(productId) {
-        try {
-            await client.$connect()
-            const res = await client.productBlocks.deleteMany({
-                where: {
-                    productId: productId
-                }
-            })
-            await client.$disconnect()
-
-            return res
-        } catch (e) {
-            console.error(e)
-            await client.$disconnect()
-        }
-    }
-
-    async updateBlock(id, type, content, productId) {
-        try {
-            await client.$connect()
-            const res = await client.productBlocks.update({
-                where: {
-                    id: id
-                },
-                data: {
-                    type: type,
-                    content: content,
-                    productId: productId
-                }
-            })
-            await client.$disconnect()
-
-            return res
-        } catch (e) {
-            console.error(e)
-            await client.$disconnect()
-        }
-    }
-
-    async updateBlocks(data) {
-        try {
-            var res = []
-            data.forEach(cur => {
-                res.push(this.updateBlock(parseInt(cur.id), cur.type, cur.content, cur.productId))
-            })
-
-            return res.length
-        } catch (e) {
-            console.error(e)
-        }
-    }
+  }
 }
 
-module.exports = new BlocksModel()
+module.exports = new BlocksModel();
